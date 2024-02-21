ed3a43f16607bb0fd7162e26ccab986b95526fce
Ana Carolina <anacarolina.silva@onfly.com.br>
2024-02-21 07:36:29 -0300

    consegui pegar resposta do body e comecei a fazer a tratativa da resposta

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('/testeDeRota')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-  
+  @Post()
+  async pegarImagens(@Body() dadosDaImagem: any) {
+    // console.log(dadosDaImagem)
+    // foreach()
+    dadosDaImagem.images.forEach(element => {
+      console.log(element)
+    });
+    // const response = await this.httpService.axiosRef({
+    //   url: 'https://example.com/image.png',
+    //   method: 'GET',
+    //   responseType: 'stream',
+    // });
+    return dadosDaImagem
+  }
 }
