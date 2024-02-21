8e6bfd0081e2a1624ff6d04a15edbece030d03c2
Ana Carolina <anacarolina.silva@onfly.com.br>
2024-02-21 08:10:24 -0300

    Testei internamente para ver como funciona a busca por dados em outro lugar

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,39 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Injectable, Post, } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MeuValor } from './variavel';
 
 @Controller('/testeDeRota')
+
 export class AppController {
   constructor(private readonly appService: AppService) { }
+
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
   @Post()
-  async pegarImagens(@Body() dadosDaImagem: any) {
+  async pegarVariavel(): Promise<string> {
+    try {
+      console.log("Esta guardado", MeuValor)
+      return MeuValor
+    } catch (error) {
+    console.error('Erro ao pegar o valor da variável:', error);
+    throw error;
+  }
+}
+
+}
+// async pegarImagens(@Body() dadosDaImagem: any) {
     // console.log(dadosDaImagem)
     // foreach()
-    dadosDaImagem.images.forEach(element => {
-      console.log(element)
-    });
+    // dadosDaImagem.images.forEach(element => {
+    //   console.log(element)
+    // });
     // const response = await this.httpService.axiosRef({
     //   url: 'https://example.com/image.png',
     //   method: 'GET',
     //   responseType: 'stream',
     // });
-    return dadosDaImagem
-  }
-}
+
+    // return dadosDaImagem
